38c79d3f0753194db7eff8abd1c61bd0de643023
Vinicius Costa <vinicius.dsc95@gmail.com>
2026-08-29 00:06:59 -0300

    chore: remove netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,14 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import svgr from "vite-plugin-svgr";
-import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(), netlify()],
+  plugins: [react(), tailwindcss(), svgr()],
   assetsInclude: ["**/*.docx"],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@lib": path.resolve(__dirname, "./src/lib"),
     },
   },
 });
