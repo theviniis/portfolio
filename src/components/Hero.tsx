3e8473d00f10653a7ca572b388dfa9e63a9f765c
Vinicius Costa <vinicius.dsc95@gmail.com>
2026-08-31 21:38:13 -0300

    feat(i18n): update Hero to use useTranslation hook

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Pointer } from "lucide-react";
+import { useTranslation } from "react-i18next";
 import { Button } from "./ui/button";
 import { ButtonGroup } from "./ui/button-group";
 
@@ -6,28 +7,29 @@ import profile from "../assets/profile.png";
 import { Section } from "./ui/Section";
 import { LinkedInButton } from "./ui/linkedin-button";
 import { GitHubButton } from "./ui/github-button";
-import heroData from "../data/hero.json";
 
 const Hero = () => {
+  const { t } = useTranslation();
+
   return (
     <Section>
       <div className="grid place-content-center gap-y-8 lg:gap-y-16">
         <div className="space-y-2 lg:space-y-4">
           <div>
-            <span className="text-h3">{heroData.greeting}</span>
-            <h1>{heroData.name}</h1>
+            <span className="text-h3">{t('hero.greeting')}</span>
+            <h1>{t('hero.name')}</h1>
           </div>
           <p>
-            {heroData.descriptionPrefix}
-            <strong>{heroData.location}</strong>.
+            {t('hero.descriptionPrefix')}
+            <strong>{t('hero.location')}</strong>.
             <br />
-            {heroData.descriptionSuffix}
+            {t('hero.descriptionSuffix')}
           </p>
         </div>
         <div className="flex items-center gap-4">
           <Button size="lg" className="pe-1 " asChild>
             <a href="#contact">
-              <span>{heroData.cta}</span>
+              <span>{t('hero.cta')}</span>
               <Button asChild variant="secondary" size="icon-sm">
                 <span>
                   <Pointer className="text-primary" />
@@ -44,7 +46,7 @@ const Hero = () => {
       <div className="bg-secondary rounded-4xl overflow-hidden relative aspect-square lg:aspect-3/4">
         <img
           src={profile}
-          alt={heroData.alt}
+          alt={t('hero.alt')}
           className="drop-shadow absolute bottom-0"
         />
       </div>
